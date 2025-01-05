4928d1f017081224a19c22103c58c21fe8cccb99
Maddison Hellstrom <maddy@na.ai>
2025-01-04 23:17:32 -0800

    chore: formatting

--- include/chayang.h
+++ include/chayang.h
@@ -7,41 +7,41 @@
 #include <wayland-client-core.h>
 
 struct chayang {
-	struct wl_compositor *compositor;
-	struct zwlr_layer_shell_v1 *layer_shell;
-	struct wp_viewporter *viewporter;
-	struct wp_single_pixel_buffer_manager_v1 *single_pixel_buffer_manager;
+  struct wl_compositor *compositor;
+  struct zwlr_layer_shell_v1 *layer_shell;
+  struct wp_viewporter *viewporter;
+  struct wp_single_pixel_buffer_manager_v1 *single_pixel_buffer_manager;
 
-	struct wl_list outputs;
-	struct wl_list seats;
+  struct wl_list outputs;
+  struct wl_list seats;
 
-	bool running, cancelled;
-	int64_t delay_ms, start_time_ms;
+  bool running, cancelled;
+  int64_t delay_ms, start_time_ms;
 };
 
 struct chayang_output {
-	struct wl_output *wl;
-	struct chayang *chayang;
-	uint32_t global_name;
-	struct wl_list link;
+  struct wl_output *wl;
+  struct chayang *chayang;
+  uint32_t global_name;
+  struct wl_list link;
 
-	struct wl_surface *surface;
-	struct zwlr_layer_surface_v1 *layer_surface;
-	struct wp_viewport *viewport;
-	struct wl_callback *frame_callback;
+  struct wl_surface *surface;
+  struct zwlr_layer_surface_v1 *layer_surface;
+  struct wp_viewport *viewport;
+  struct wl_callback *frame_callback;
 
-	int32_t surface_width, surface_height;
+  int32_t surface_width, surface_height;
 };
 
 struct chayang_seat {
-	struct wl_seat *wl;
-	struct chayang *chayang;
-	uint32_t global_name;
-	struct wl_list link;
+  struct wl_seat *wl;
+  struct chayang *chayang;
+  uint32_t global_name;
+  struct wl_list link;
 
-	uint32_t caps;
-	struct wl_pointer *pointer;
-	struct wl_keyboard *keyboard;
+  uint32_t caps;
+  struct wl_pointer *pointer;
+  struct wl_keyboard *keyboard;
 };
 
 #endif
